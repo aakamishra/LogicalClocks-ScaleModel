fc70f33dafb9b94c8432f3b5ff3118e37a1630b8
Rajat Mittal <rajatmittal@college.harvard.edu>
2023-03-07 23:45:21 -0500

    fixed bugs

--- p2p_simulator.py
+++ p2p_simulator.py
@@ -67,6 +67,7 @@ class VirtualMachine:
         
         # create logical clock for this machine
         self.clock = LogicalClock()
+        self.last_time = 0
         
         
         
@@ -94,30 +95,36 @@ class VirtualMachine:
         
         time.sleep(3)
         
+        # localhost
+        host= "127.0.0.1"
+
         # extensible to multiple producers
-        prods = []
+        sockets_dict = {}
         agent = 0
         for i in range(len(self.ports_list)):
             port = self.ports_list[i]
             if self.p_val != (i + 1):
                 agent += 1
-                prod_thread = Thread(target=self.producer, args=(port, agent, i+1,))
-                prods.append(prod_thread)
-                prod_thread.start()
+                port = int(port)
+                s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                sockets_dict[agent] = (s, i)
+                try:
+                    s.connect((host, port))
+                except Exception as e:
+                    print(e)
+                
 
-        start_time = last_time = time.time()
+        start_time = self.last_time = time.time()
         
         while True:
             new_time = time.time()
-            elapsed_time = new_time - last_time
+            elapsed_time = new_time - self.last_time
             
             if new_time - start_time > 65:
-                for t in prods:
-                    t.join()
                 return 
             
             if elapsed_time >= 1/self.speed:
-                last_time = new_time
+                self.last_time = new_time
                 
                 self.op_code = (random.randint(1,10), self.clock.counter)
                 val = -1
@@ -134,7 +141,8 @@ class VirtualMachine:
                             event_type="RECEIVED",
                             post_queue_length=n,
                             sender=id,
-                            reciever=self.p_val
+                            reciever=self.p_val,
+                            description=self.op_code[0]
                         ).update(self.lgr)
                     elif self.op_code[0] >= 4:
                         internal = f"{self.p_val},{self.op_code}"
@@ -146,6 +154,34 @@ class VirtualMachine:
                             post_queue_length=n,
                             description=self.op_code[0]
                         ).update(self.lgr)
+                    elif self.op_code[0] in sockets_dict.keys():
+                        s, id = sockets_dict[self.op_code[0]]
+                        msg = f"{self.p_val},{self.clock.counter}"
+                        s.send(msg.encode('ascii'))
+                        LogData(
+                            id=self.p_val,
+                            clock_counter=self.clock.counter,
+                            event_type="SEND",
+                            post_queue_length=n,
+                            sender=self.p_val,
+                            reciever=id,
+                            description=self.op_code[0]
+                        ).update(self.lgr)
+                    else:
+                        for agent in sockets_dict.keys():
+                            s, id = sockets_dict[agent]
+                            msg = f"{self.p_val},{self.clock.counter}"
+                            s.send(msg.encode('ascii'))
+                            LogData(
+                                id=self.p_val,
+                                clock_counter=self.clock.counter,
+                                event_type="SEND",
+                                post_queue_length=n,
+                                sender=self.p_val,
+                                reciever=id,
+                                description=self.op_code[0]
+                            ).update(self.lgr)
+
                 self.clock.update(int(val))
             
     
@@ -197,58 +233,7 @@ class VirtualMachine:
                 id, val = data_value[0], data_value[1]
                 with self.queue_lock:
                     self.queue.append((id, val))
-    
-    def producer(self, port, agent, recv_p_val):
-        host= "127.0.0.1"
-        port = int(port)
-        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         
-        try:
-            s.connect((host,port))
-            
-            start_time = last_time = time.time()
-            while True:
-                new_time = time.time()
-                elapsed_time = new_time - last_time
-
-                if time.time() - start_time > 65:
-                    return 
-                
-                # if elapsed_time < 1/self.speed:
-                #     continue
-
-                # last_time = new_time
-                n = 0
-                with self.queue_lock:
-                    n = len(self.queue)
-                
-                    if n == 0:
-                        code_value, clock_time = self.op_code
-
-                        if agent == code_value or code_value == 3 and code_value != 0:
-                            msg = f"{self.p_val},{self.clock.counter}"
-                            # sent = f"{self.p_val}->{recv_p_val}"
-                            # self.update(self.p_val, "SEND", sent)
-                            queue_length = None
-                            s.send(msg.encode('ascii'))
-                            LogData(
-                                id=self.p_val,
-                                clock_counter=clock_time,
-                                event_type="SEND",
-                                post_queue_length=n,
-                                sender=self.p_val,
-                                reciever=recv_p_val,
-                            ).update(self.lgr)
-                time.sleep(1/self.speed)
-
-
-        except socket.error as e:
-            print ("Error connecting producer: %s" % e)
-        
-    # def update(self, id, event_type, desc, queue_length=None):    
-    #     info_str = str(id) + "," + str(time.time_ns()) + "," + str(self.clock.counter) + "," + str(event_type) + "," + str(desc) + "," + str(queue_length)  
-    #     print(info_str)      
-    #     self.lgr.info(info_str)
 
 localHost= "127.0.0.1"
  
@@ -261,6 +246,7 @@ if __name__ == '__main__':
     machine1 = VirtualMachine(config1)
 
     folder_id = random.randint(1, 10000)
+    print(f"Folder ID: {folder_id}")
     os.mkdir(f"logs/logs_{folder_id}")
 
     p1 = Process(target=machine1.run, args=(folder_id,))
